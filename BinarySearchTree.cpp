14dd5152e23f09ed315d12c2d2962d13bc8d5b04
HafizZada16 <hafizzada16@gmail.com>
2024-06-13 18:36:53 +0700

    Add if for (If the parent is NULL (Tree is empty) -> Mark the new node as ROOT -> Exit )

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -36,6 +36,12 @@ public:
         Node* parent = nullptr;
         Node* currentNode = nullptr;
         search(element, parent, currentNode); // Locate the node which will be the parent of the node to be insert
+
+        if (parent == nullptr) // If the parent is NULL (Tree is empty)
+        {
+            ROOT = newNode; // Mark the new node as ROOT
+            return; // Exit
+        }
     }
 
 };
